19c7604342d81a2a85916214cefa6c629d31103b
PTBZYN <119863589+PTBZYN@users.noreply.github.com>
2022-12-05 20:58:19 +0700

    Update config.js
    Signed-off-by: PTBZYN <119863589+PTBZYN@users.noreply.github.com>

--- config.js
+++ config.js
@@ -3,18 +3,18 @@ const CONFIG = {
     introTitle: 'BÉ ZYN',
     introDesc: `Thật sự a rất cảm ơn em
     Đã chấp nhận yêu anh
-    Đã đến bên anh, yêu anh, lo cho anh và luôn bên anh
-    Trải qua được 1 khoảng thời gian yêu nhau, a đã khóc rất nhiều vì em
-    Em đã không bỏ a vì a cứ hay mít ướt, lại còn vì anh mà nói dối mẹ cơ
+    Đã đến bên anh yêu anh lo cho anh và luôn bên anh
+    Trải qua được 1 khoảng thời gian yêu nhau a đã khóc rất nhiều vì em
+    Em đã không bỏ a vì a cứ hay mít ướt lại còn vì anh mà nói dối mẹ cơ
     Hay gần đây là việc a sợ a rớt môn em còn đòi bán cả acc game để cho a học
-    Rồi đến cả việc em nấu ăn cho anh, em làm rất nhiều thứ cho 
+    Rồi đến cả việc em nấu ăn cho anh em làm rất nhiều thứ cho 
     Anh cảm ơn em đã yêu anh`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Anh mong em và anh sẽ được ở bên nhau, em ráng chờ a nhé? <3',
+    question: 'Anh mong em và anh sẽ được ở bên nhau em ráng chờ a nhé?',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu em nhiều nhiều 😘😘',
